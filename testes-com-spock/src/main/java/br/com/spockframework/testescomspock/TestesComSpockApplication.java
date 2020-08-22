0a807167f9fa0ae26de47327210b7a438636c614
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2020-08-22 17:21:00 -0300

    commit initial

---
+++ testes-com-spock/src/main/java/br/com/spockframework/testescomspock/TestesComSpockApplication.java
@@ -0,0 +1,13 @@
+package br.com.spockframework.testescomspock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesComSpockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestesComSpockApplication.class, args);
+	}
+
+}
